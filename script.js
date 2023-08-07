aae9cf33c8bb7a2b3795b8b53c4fcc7363c03210
FrankJuanI <juan.frank@gm2dev.com>
2023-08-07 12:38:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+function calculateTotalPrice(precio, descuento) {
+  const discountToApply = (precio * descuento) / 100;
+  const totalPrice = precio - discountToApply;
+  return totalPrice;
+}
+
+function calculateCuotaPrice(precio) {
+  const precioCuota = precio / 12;
+  return precioCuota;
+}
+
+function producto(titulo, precio, descuento, descripcion) {
+  this.titulo = titulo;
+  this.precio = precio;
+  this.descuento = `El descuento es de ${descuento}`;
+  precioConDescuento = `Precio con descuento $ ${calculateTotalPrice(
+    precio,
+    descuento
+  )}`;
+  precioCuotas = `Hasta 12 cuotas sin descuento: precio de cuota $${calculateCuotaPrice}`;
+  this.descripcion = descripcion;
+}
+
+var producto1 = new producto(
+  "MacBook",
+  1500,
+  10,
+  "La macBook es nueva, cuenta con 16gb de ram y u chip M1"
+);
+
+var producto2 = new producto(
+  "PadMouse Logitech",
+  500,
+  25,
+  "Es excelente para que puedas pegar varios HEADSHOTS"
+);
+
+var producto3 = new producto(
+  "Joystick",
+  3500,
+  35,
+  "Tremendo Jpystick XBOX para ganarle a tu amigo al fifa"
+);

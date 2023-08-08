eb6de38b300efb5d840591c26e63a2dc17dc766f
FrankJuanI <juan.frank@gm2dev.com>
2023-08-08 11:00:32 -0300

    Tarea Objetos Array

--- script.js
+++ script.js
@@ -1,47 +1,84 @@
-function calculateTotalPrice(precio, descuento) {
-  const discountToApply = (precio * descuento) / 100;
-  const totalPrice = precio - discountToApply;
-  return totalPrice;
+class cProducto {
+  constructor(titulo, precio, descuento, descripcion) {
+    (this.titulo = titulo),
+      (this.precio = precio),
+      (this.descuento = `El descuento es de ${descuento}`),
+      (this.precioConDescuento = `Precio con descuento $ ${this.calculateTotalPrice(
+        precio,
+        descuento
+      )}`),
+      (this.precioCuotas = `Hasta 12 cuotas sin descuento: precio de cuota $${this.calculateCuotaPrice()}`),
+      (this.descripcion = descripcion);
+  }
+
+  calculateTotalPrice() {
+    const discountToApply = (this.precio * this.descuento) / 100;
+    const totalPrice = this.precio - discountToApply;
+    return totalPrice;
+  }
+
+  calculateCuotaPrice() {
+    const precioCuota = this.precio / 12;
+    return precioCuota;
+  }
 }
 
-function calculateCuotaPrice(precio) {
-  const precioCuota = precio / 12;
-  return precioCuota;
+function crearProducto(titulo, precio, descuento, descripcion) {
+  let producto = {
+    titulo: titulo,
+    precio: precio,
+    descuento: descuento,
+    precioConDescuento: `El descuento es de ${calcularPrecioTotal()}`,
+    precioCuotas: `Precio cuotas ${calcularPrecioCuota()}`,
+    descripcion: descripcion,
+  };
+
+  function calcularPrecioTotal() {
+    const discountToApply = (this.precio * this.descuento) / 100;
+    const totalPrice = this.precio - discountToApply;
+    return totalPrice;
+  }
+
+  function calcularPrecioCuota() {
+    const precioCuota = this.precio / 12;
+    return precioCuota;
+  }
+  return producto;
 }
 
-function producto(titulo, precio, descuento, descripcion) {
-  this.titulo = titulo;
-  this.precio = precio;
-  this.descuento = `El descuento es de ${descuento}`;
-  precioConDescuento = `Precio con descuento $ ${calculateTotalPrice(
-    precio,
-    descuento
-  )}`;
-  precioCuotas = `Hasta 12 cuotas sin descuento: precio de cuota $${calculateCuotaPrice}`;
-  this.descripcion = descripcion;
-}
+productos = [];
 
-var producto1 = new producto(
+const producto1 = new cProducto(
   "MacBook",
   1500,
   10,
   "La macBook es nueva, cuenta con 16gb de ram y u chip M1"
 );
 
-var producto2 = new producto(
+const producto2 = new cProducto(
   "PadMouse Logitech",
   500,
   25,
   "Es excelente para que puedas pegar varios HEADSHOTS"
 );
 
-var producto3 = new producto(
+const producto3 = new cProducto(
   "Joystick",
   3500,
   35,
   "Tremendo Jpystick XBOX para ganarle a tu amigo al fifa"
 );
 
-console.log(producto1);
-console.log(producto2);
-console.log(producto3);
+const producto4 = crearProducto(
+  "Mancuerna",
+  500,
+  10,
+  "Terrible herramienta para pegarle a los tacheros"
+);
+
+productos.push(producto1, producto2, producto3, producto4);
+
+console.log(productos[0]);
+console.log(productos[1]);
+console.log(productos[2]);
+console.log(productos[3]);
